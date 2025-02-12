5ea396e9702909e14c5c41b73de35f729d0ffb28
Mosman1707 <c4026502@hallam.shu.ac.uk>
2025-02-12 10:57:47 +0000

    nasa update

--- weather/urls.py
+++ weather/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView, weather_view
+from .views import HomeView, weather_view, nasa_data
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('weather/', weather_view, name='weather_view'),
-   
+    path('nasa_data/', nasa_data, name='nasa_data'),   
 ]
--- weather/views.py
+++ weather/views.py
@@ -17,7 +17,7 @@ def weather_view(request):
   if request.method == 'GET' and 'lat' in request.GET and 'lon' in request.GET:
     lat = request.GET['lat']
     lon = request.GET['lon']
-    api_key = '<your API key>'  # Replace with your OpenWeatherMap API key
+    api_key = ''  # Replace with your OpenWeatherMap API key
     units = 'metric'
     lang = 'fr'
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units={units}&lang={lang}'
@@ -30,4 +30,24 @@ def weather_view(request):
       return JsonResponse({'error': 'Unable to fetch weather data'}, status=500)
   else:
     return JsonResponse({'error': 'Invalid request method or missing coordinates'}, status=400)
+  
+
+
+
+
+
+def nasa_data(request):
+    api_key = 'Wk9vdjuaB7s1Q24nSIA944027BNdWS2kGpqsAhPT'
+    url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Will raise an exception for bad status codes (400+)
+        data = response.json()
+    except requests.exceptions.RequestException as e:
+        # Handle errors gracefully
+        data = {'error': str(e)}
+
+    return render(request, 'weather/nasa.html', {'nasa_data': data})
+
 

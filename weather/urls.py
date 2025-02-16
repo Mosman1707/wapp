eb428c5e61a476f0b40a0fd03c6473751ebb64aa
Mosman1707 <c4026502@hallam.shu.ac.uk>
2025-02-16 19:23:15 +0000

    debug ow api and nasa page

--- weather/urls.py
+++ weather/urls.py
@@ -6,5 +6,5 @@ from .views import HomeView, weather_view, nasa_data
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('weather/', weather_view, name='weather_view'),
-    path('nasa_data/', nasa_data, name='nasa_data'),   
+    path('weather/nasa_data/', nasa_data, name='nasa_data'),   
 ]
